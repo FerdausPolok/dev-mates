5f9ca3ccfed3cf4a03ed406a3d27d32fc2190c6a
Aaryadotpy <91911418+AaryaNale@users.noreply.github.com>
2023-10-07 10:54:54 +0530

    Update To_Do.py
    ##Additions:
    - Added "Add a task" placeholder to the input textbox
    - Now you can add tasks to the list with the 'Add Task' button provided, or simply press the 'Enter' key to do so
    - Right click removes a task from the task list
    - Added a scroll bar to deal with a large number of tasks
    - Improved the paddingof the elements to make the window look more user-friendly
    - Wrapped the "Instructions" text

--- To-Do-List/To_Do.py
+++ To-Do-List/To_Do.py
@@ -1,30 +1,34 @@
 import tkinter as tk
+from tkinter import messagebox
 
 # Function to show an error message window
 def show_error_window(message):
-    error_window = tk.Toplevel(root)
-    error_window.title("Error")
-    error_label = tk.Label(error_window, text=message)
-    error_label.pack(padx=20, pady=20)
-    close_button = tk.Button(error_window, text="Close", command=error_window.destroy)
-    close_button.pack(pady=10)
+    messagebox.showerror("Error", message)
 
 # Function to open the instruction window
 def open_instruction_window():
+    instruction_text = """\
+    --- Welcome to the To-Do List Application! ---
+
+    Instructions:
+    1. Enter a task in the input field below.
+    2. Press 'Enter' or click 'Add Task' to add the task to your to-do list.
+    3. Click on a task to mark it as completed (✓) or click on it again
+        to mark it incomplete.
+    4. Right-click on a task to delete it.
+    5. Enjoy organizing your tasks!
+    """
     instruction_window = tk.Toplevel(root)
     instruction_window.title("Instructions")
-    instruction_label = tk.Label(instruction_window, text="---Welcome to the To-Do List Application!---\n\n"
-                                                        "Instructions:\n"
-                                                        "1. Enter a task in the input field below.\n"
-                                                        "2. Click 'Add Task' to add the task to your to-do list.\n"
-                                                        "3. Click on a task to mark it as completed (✓) or click on it again to mark it incomplete.\n"
-                                                        "4. Enjoy organizing your tasks!")
+    
+    instruction_label = tk.Label(instruction_window, text=instruction_text, justify="left", wraplength=400)
     instruction_label.pack(padx=20, pady=20)
+    
     close_button = tk.Button(instruction_window, text="Close", command=instruction_window.destroy)
     close_button.pack(pady=10)
 
 # Function to add tasks
-def add_task():
+def add_task(event=None):
     task = entry.get().strip()  # Remove leading/trailing whitespace
     if task:
         listbox.insert(tk.END, task)
@@ -45,6 +49,12 @@ def toggle_task(event):
         listbox.delete(selected_task_index)
         listbox.insert(selected_task_index, task)
 
+# Function to delete a task
+def delete_task(event):
+    selected_task_index = listbox.nearest(event.y)
+    if selected_task_index >= 0:
+        listbox.delete(selected_task_index)
+
 # Create the main window
 root = tk.Tk()
 root.title("To-Do List")
@@ -53,20 +63,33 @@ root.title("To-Do List")
 instruction_button = tk.Button(root, text="Instructions", command=open_instruction_window)
 instruction_button.pack(pady=10)
 
-# Create a listbox to display tasks
-listbox = tk.Listbox(root, selectmode=tk.SINGLE)
-listbox.pack(pady=10)
+# Create a listbox to display tasks with a scrollbar
+listbox_frame = tk.Frame(root)
+listbox_frame.pack(pady=10, padx=20, fill=tk.BOTH, expand=True)
 
-# Create an entry field to add tasks
-entry = tk.Entry(root)
-entry.pack(pady=5)
+listbox = tk.Listbox(listbox_frame, selectmode=tk.SINGLE, selectbackground="lightblue", selectforeground="black", font=("Arial", 12))
+listbox.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+
+scrollbar = tk.Scrollbar(listbox_frame)
+scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+
+listbox.config(yscrollcommand=scrollbar.set)
+scrollbar.config(command=listbox.yview)
+
+# Create an entry field to add tasks with a placeholder
+entry = tk.Entry(root, font=("Arial", 12), justify=tk.LEFT)
+entry.insert(0, "Add a task...")
+entry.bind("<FocusIn>", lambda event: entry.delete(0, tk.END))  # Clear placeholder when focused
+entry.bind("<Return>", add_task)  # Bind the 'Return' key to add_task function
+entry.pack(pady=5, padx=20, fill=tk.BOTH, expand=True)
 
 # Create a button to add tasks
-add_button = tk.Button(root, text="Add Task", command=add_task)
-add_button.pack(pady=5)
+add_button = tk.Button(root, text="Add Task", command=add_task, font=("Arial", 12))
+add_button.pack(pady=5, padx=20)
 
 # Bind the listbox to the toggle_task function
 listbox.bind('<ButtonRelease-1>', toggle_task)
+listbox.bind('<Button-3>', delete_task)  # Right-click to delete task
 
 # Start the tkinter main loop
 root.mainloop()
